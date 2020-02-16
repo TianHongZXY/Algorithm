8f091f1a9ab44164cd4ef48b1b465a1ec00f8544
xinyuzhu <xinyu.zhu@shanbay.com>
2020-02-16 19:11:19 +0800

    init

---
+++ 1-1.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+const int MAX_N = 50;
+
+int main(){
+    int n, m, k[MAX_N];
+    k[0] = 10;
+    int x = 10;
+    cout << "i love cj" << endl;
+}
