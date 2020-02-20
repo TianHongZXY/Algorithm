a1383bad3d68abcc0f53ec74017f471e985d182f
xinyuzhu <xinyu.zhu@shanbay.com>
2020-02-20 22:16:48 +0800

    2.20

---
+++ 挑战程序设计竞赛/chapter 2/2-2-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX_N = 100;
+int N, S[MAX_N], T[MAX_N];
+pair<int, int> itv[MAX_N];
+
+void solve(){
+    for (int i = 0; i < N; i++){
+        itv[i].first = T[i];
+        itv[i].second = S[i];
+    }
+    sort(itv, itv + N);
+
+    int ans = 0, t = 0;
+    for (int i = 0; i < N; i++)
+        if (t < itv[i].second){
+            t = itv[i].first;
+            ans++;
+        }
+    cout << ans << endl;
+}
+
+int main(){
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cin >> S[i];
+    }
+    for (int i = 0; i < N; i++){
+        cin >> T[i];
+    }
+    solve();
+    return 0;
+}
---
+++ 挑战程序设计竞赛/chapter 2/POJ-3617_Best_Cow_Line.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const int MAX_N = 2000;
+int N;
+char S[MAX_N + 1];
+
+void solve(){
+    int a = 0, b = N - 1;
+    int ans = 0;
+    while (a <= b){
+        bool left = false;
+        for (int i = 0; a + i <= b; i++)
+            if (S[a + i] < S[b - i]){
+                left = true;
+                break;
+            }
+            else if (S[a + i] > S[b - i]){
+                left = false;
+                break;
+            }
+        if (left) cout << S[a++];
+        else cout << S[b--];
+        ans += 1;
+        if(ans % 80 == 0)
+            cout << endl;
+    }
+}
+
+int main(){
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> S[i];
+    solve();
+    return 0;
+}
