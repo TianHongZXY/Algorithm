7230d538cfaafcbebbd5918cffb42f250c890fbe
xinyuzhu <xinyu.zhu@shanbay.com>
2020-02-25 00:32:01 +0800

    go deeper to const & add cmakelists

--- c++ primer/chapter 2/const_qualifier.cpp
+++ c++ primer/chapter 2/const_qualifier.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
     // const int k;  // must be initialized
 
     // const can be used to initialize other const or non-const
-    const int j = bufSize; 
+    const int j = bufSize;
     int l = bufSize;
     l = 42;
     const int m = l;
@@ -36,6 +36,46 @@ int main(int argc, char const *argv[])
     // int &rd = dval; // not allowed if rd isn't const and reference to a different type.
     cout << rd << endl;
     // rd = 4; // rd is bound to a temporary obj,
+
+    int k = 33;
+    int &rk = k;
+    const int &rkk = k;
+    rk = 55;
+//    rkk = 66; // not allowed reference to const can refer to non-const, but can't change it
+
+//  Pointer to const
+    const double pi = 3.14;
+//    double *ptr = &pi; // error, pointer must be const too
+    const double *cptr = &pi;
+//    *cptr = 9.99 // error, can't assign to *cptr
+
+    cptr = &dval; // const pointer can point to non-const obj
+
+//  const Pointer
+    int errNumb = 0;
+    int *const curErr = &errNumb; // curErr will always point to errNumb
+    const double PI = 3.14159;
+    const double *const pip = &PI; // pip is a const pointer to a const obj
+
+//    *pip = 3.0 // error, pip points to a const obj
+    if (*curErr){
+//      const pointer can change the value of obj it points to if the obj itself is non-const
+//      no matter whether the pointer is const or not
+        *curErr = 10;
+    }
+
+//    when copy an obj, top-level consts are ignored
+//    copy an obj doesn't change the copied obj
+    int q = 0;
+    const int r = 10;
+    q = r; // q copy r's value, r's top-level const is ignored
+    cout << q << endl;
+    q = 9; // q is still plain int
+
+    const int *p1 = &q;
+    const int *const p2 = &r;
+    p1 = p2; // p2 is a const pointer, p1 copies p2's value, e.g. r's address
+    p1 = &q; // p1 is still a plain pointer
 
     return 0;
 }
